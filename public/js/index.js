b7fc4942e0f0736cd814542cdbcfe9118621f645
Fabricio Mendonca <fabriciomendonca@gmail.com>
2017-04-03 14:58:45 -0300

    Greet new user and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-  socket.emit('createMessage', {
-    from: 'Andrew',
-    text: 'Test text',
-    createdAt: 123
-  });
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -13,15 +13,30 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New user connected');
-  
   socket.emit('newMessage', {
-    from: 'test@test.com',
-    text: 'test text',
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user has joined the room',
     createdAt: new Date().getTime()
   });
 
   socket.on('createMessage', message => {
     console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+    /*socket.broadcast.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    })*/
   });
 
   socket.on('disconnect', () => {
